299c38d696d653b57287732ad6f9776714fd1672
iqrafatimame <iqrafatimame@gmail.com>
2021-11-24 16:48:36 +0500

    🐛 Clear Input field after evaluation

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-const screen = document.getElementById('screen');
-const buttons =  Array.from(document.getElementsByTagName('span'))
+const screen = document.getElementById('screen')
+const buttons = Array.from(document.getElementsByTagName('span'))
+// Flag to check if the text in the input field is evaluated of just an expression
+let isEvaluated = false
 
 buttons.map( button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText){
             case 'C':
-                screen.innerText = '';
-                break;
+                screen.innerText = ''
+                break
             case '=':
                 try{
-                    screen.innerText = eval(screen.innerText); 
+                    screen.innerText = eval(screen.innerText)
                 }
                 catch{
-                    screen.innerText = 'Error!';
+                    screen.innerText = 'Error!'
                 }
-                break;
+                isEvaluated = true
+                break
             default:
-                screen.innerText+=e.target.innerText;
+                if(isEvaluated){
+                    screen.innerText = ''
+                    isEvaluated = false
+                }
+                screen.innerText+=e.target.innerText
         }
     })
 })
-
-
-
-
